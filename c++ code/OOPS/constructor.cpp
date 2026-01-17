88cf0bda1e65efda21570acfc9ae0cb77ad308cc
Tarang <tarangsuryawanshitarang@gmail.com>
2026-01-17 17:50:52 +0530

    constructor

---
+++ c++ code/OOPS/constructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class student {
+    public : 
+    string name;
+    int rollno;
+    // constructor
+    student ( string s , int r){
+        name = s;
+        rollno = r;
+    }
+    void display()
+    {
+        cout << "name : " << name << endl;
+        cout << "rollno : " << rollno << endl;
+    }
+};
+int main() {
+  student s1("Tarang suryawanshi",70);
+  s1.display();
+}
