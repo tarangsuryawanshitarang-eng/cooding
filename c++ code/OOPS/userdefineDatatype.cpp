78e3df0d7a96ee67f7939bf9b28c102ea7e36ed5
Tarang <tarangsuryawanshitarang@gmail.com>
2026-01-18 00:21:20 +0530

    vector

---
+++ c++ code/OOPS/userdefineDatatype.cpp
@@ -0,0 +1,53 @@
+// user define data type .
+#include <iostream>
+using namespace std;
+class Vector{
+    public :
+    int size;
+    int *arr;
+    int capacity;
+    Vector(){
+        size = 0;
+        capacity = 1;
+        arr = new int [1];
+    }
+    void add(int element ){
+        if (size == capacity){
+            // create a new array
+            capacity *= 2;
+            int *newArr = new int [capacity];
+            // copy old array element to new array
+            for (int i = 0; i < size; i++){
+                newArr[i] = arr[i];
+            }
+            // delete old array
+            delete [] arr;
+            // point arr to new array
+            arr = newArr;
+        }            
+        arr[size++] = element;
+    }
+    void print(){
+        for (int i = 0; i < size; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    void remove(){
+        if (size > 0){
+            size--;
+        }
+    }
+};
+int main() {
+  Vector v;
+  v.add(1);
+  v.print();
+    v.add(2);
+    v.print();
+    v.add(3);
+    v.print();
+    v.remove();
+    v.print();
+    return 0;
+}
