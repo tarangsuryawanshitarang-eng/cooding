b1ab91060a16a9764bb252a5cbbe77b6ed34c4ef
Tarang <tarangsuryawanshitarang@gmail.com>
2026-01-18 11:14:08 +0530

    seprate  folder created

---
+++ c++ code/OOPS/polymorphism/example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class polymorphism{
+    public:
+    void sum(int a , int b){
+        cout<<"Sum of two integers : "<<a+b<<endl;
+    }
+    void sum(int a , int b , int c ){
+        cout<<"Sum of two integers : "<<a+b+c<<endl;
+    }
+    void sum(string a , string b){
+        cout<<"Sum of two strings : "<<a+b<<endl;
+    }
+    
+};
+int main() {
+    polymorphism p1;
+    p1.sum(5,10);
+    p1.sum(5,10,15);
+    p1.sum("Hello"," World");
+}
