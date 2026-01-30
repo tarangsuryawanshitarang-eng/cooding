55343dd6bd8976a998fe66d42f1609c7f979e1e3
Tarang Suryawanshi <tarangsuryawanshitarang@gmail.com>
2026-01-30 12:49:11 +0530

    del-fx

--- DSA/LINK-LIST/Delete-fx/delete-at-k-position.cpp
+++ DSA/LINK-LIST/Delete-fx/delete-at-k-position.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 using namespace std;
+
 class node{
     public:
-    int data;
+    int data ;
     node* nxt;
-    node(int val){ data = val; nxt = NULL; }
+
+    node(int val){data = val ; nxt = NULL;}
+
 };
+
 void pop_front(node* &head){
     if(head == NULL) return;
     head = head -> nxt;
 
 }
+void del(node* &head , int pos){
+    if(head == NULL) return;
+    if(pos == 0){
+        pop_front(head);
+        return;
+    }
+
+ node* temp=head;
+ for(int i = 0  ; i < pos-1 ; i++){
+    temp = temp->nxt;
+    } node* toDelete = temp->nxt;   // 30
+    temp->nxt = temp->nxt->nxt;  // 20 -> 40
+    delete toDelete;             // delete 30
+}
+
 void display(node* head){
     node* temp = head;
     while(temp != NULL){
@@ -19,8 +38,9 @@ void display(node* head){
     } cout<<"NULL";
 }
 
-int main() {
-    node* head = new node(10) ;
+
+int main(){
+     node* head = new node(10) ;
     node* a = new node(20) ;
     node* b = new node(30) ;
     node* c = new node(40) ;
@@ -30,7 +50,6 @@ int main() {
     b ->nxt = c;
 
     display(head); cout<<endl;
-    pop_front(head);
+    del(head,2);
     display(head);
-
 }
